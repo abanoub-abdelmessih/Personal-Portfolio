ab9d95c60f272075b876eefb57f6f9d622fe95f3
Abanoub-Abd-Elmessih <abanoubabdelmessih110@gmail.com>
2025-06-30 01:16:58 +0300

    add image fallback

--- src/components/hero/HeroImage.jsx
+++ src/components/hero/HeroImage.jsx
@@ -1,11 +1,14 @@
 export const HeroImage = () => {
   return (
     <div className="border border-gray-400 p-3 rounded-full shadow-sm/70">
-      <img
-        src="/images/abanoub.webp"
-        alt="Abanoub profile photo"
-        className="rounded-full w-48 h-48 md:w-56 md:h-56 object-cover object-top grayscale hover:grayscale-0 duration-300"
-      />
+      <picture>
+        <source srcSet="/images/abanoub.webp" type="image/webp" />
+        <img
+          src="/images/abanoub.jpg"
+          alt="Abanoub profile photo"
+          className="rounded-full w-48 h-48 md:w-56 md:h-56 object-cover object-top grayscale hover:grayscale-0 duration-300"
+        />
+      </picture>
     </div>
   );
 };
