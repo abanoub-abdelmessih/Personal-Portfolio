3721d568421d39b7fda6d685b71f111b77bc75dc
abanoub-abdelmessih <abanoubabdelmessih110@gmail.com>
2025-09-29 12:40:47 +0300

    add shadcn components

---
+++ src/components/Card.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardHeader,
+  CardDescription,
+  CardTitle,
+  CardFooter,
+  CardContent,
+} from "@/components/ui/card";
+
+const OverlayCard = () => {
+  return (
+    <div className="relative max-w-md rounded-xl bg-gradient-to-r from-neutral-600 to-violet-300 pt-0 shadow-lg">
+      <div className="flex h-60 items-center justify-center">
+        <img
+          src="https://cdn.shadcnstudio.com/ss-assets/components/card/image-11.png?width=300&format=auto"
+          alt="Shoes"
+          className="w-75"
+        />
+      </div>
+      <Card className="border-none">
+        <CardHeader>
+          <CardTitle>Nike Jordan Air Rev</CardTitle>
+          <CardDescription className="flex items-center gap-2">
+            <Badge variant="outline">EU38</Badge>
+            <Badge variant="outline">Black and White</Badge>
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>
+            Crossing hardwood comfort with off-court flair. &apos;80s-Inspired
+            construction, bold details and nothin&apos;-but-net style.
+          </p>
+        </CardContent>
+        <CardFooter className="justify-between gap-3 max-sm:flex-col max-sm:items-stretch">
+          <div className="flex flex-col">
+            <span className="text-sm font-medium uppercase">Price</span>
+            <span className="text-xl font-semibold">$69.99</span>
+          </div>
+          <Button size="lg">Add to cart</Button>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+};
+
+export default OverlayCard;
